4e3bbd65d2c7c544e747c0c19ba280b27f280ccc
tweinacker57 <tweinacker57@gmail.com>
2021-02-28 12:39:11 -0500

    Add new line symbols where necessary. Remove endl

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,9 +2,8 @@
 int main()
 {
     //Create the start of a game that asks an adventurer to solve the puzzle based on the sum and products of the variables provided
-    std::cout << "You are an adventurer who needs to solve the puzzle to open the mystical portal";
-    std::cout << std::endl;
-    std::cout << "Enter the correct numbers to solve the puzzle to open the portal";
+    std::cout << "You are an adventurer who needs to solve the puzzle to open the mystical portal\n";
+    std::cout << "Enter the correct numbers to solve the puzzle to open the portal\n\n";
 
     const int PuzzleA = 4;
     const int PuzzleB = 3;
@@ -14,28 +13,26 @@ int main()
     const int PuzzleProduct = PuzzleA * PuzzleB * PuzzleC;
 
     // Print sum and product to the terminal
-    std::cout << std::endl;
     std::cout << "There are 3 numbers needed to solve the puzzle" << std::endl;
-    std::cout << "The numbers in the puzzle add up to: " << PuzzleSum << std::endl;
-    std::cout << "The numbers in the puzzle multiply to give: " << PuzzleProduct << std::endl;
+    std::cout << "\n+The numbers in the puzzle add up to: " << PuzzleSum << std::endl;
+    std::cout << "\n+The numbers in the puzzle multiply to give: " << PuzzleProduct << std::endl;
 
-
+    // Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    //Use if else statement to let player know if they're correct or incorrect
     if (GuessSum == PuzzleSum && GuessProduct == PuzzleProduct)
     {
-        std::cout << "You solved the puzzle!";
+        std::cout << "\nYou solved the puzzle!";
     }
 
     else
     {
-        std::cout << "You solved incorrectly...";
+        std::cout << "\nYou solved incorrectly...";
     }
     
 
