5fe6cb7665b689ea84734c162fb742e2f7032e80
tweinacker57 <tweinacker57@gmail.com>
2021-02-27 20:23:46 -0500

    Add more text for the user

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main()
 {
+    //Create the start of a game that asks an adventurer to solve the puzzle based on the sum and products of the variables provided
     std::cout << "You are an adventurer who needs to solve the puzzle to open the mystical portal";
     std::cout << std::endl;
     std::cout << "Enter the correct numbers to solve the puzzle to open the portal";
@@ -12,7 +13,9 @@ int main()
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    // Print sum and product to the terminal
     std::cout << std::endl;
+    std::cout << "There are 3 numbers needed to solve the puzzle" << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
 
