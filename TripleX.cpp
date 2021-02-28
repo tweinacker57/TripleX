1b06b0da85641be985ff68d77a320822d891f9a6
tweinacker57 <tweinacker57@gmail.com>
2021-02-27 19:52:15 -0500

    Add more text
    Add more text and a line break for the user

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,5 +2,8 @@
 int main()
 {
     std::cout << "You are an adventurer who needs to solve the puzzle to open the mystical portal";
+    std::cout << std::endl;
+    std::cout << "Enter the correct numbers to solve the puzzle to open the portal";
+
     return 0;
 }
